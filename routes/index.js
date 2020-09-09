27143edde5b55ecbe4901b9ce825c65b959e09a5
sdlckdrl <sdlckdrl@gmail.com>
2020-09-09 11:27:53 +0900

    first commit

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var path = require("path") 
+var router = express.Router();
+var multer = require('multer'); // express에 multer모듈 적용 (for 파일업로드)
+var upload = multer({ dest: 'uploads/' })
+
+router.get('/', function (req, res, next) {
+  res.sendFile(path.join(__dirname, '../public', 'index.html'));
+});
+
+router.get('/images/', function (req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/private', function (req, res, next) {
+  res.sendFile(path.join(__dirname, '../public', 'index.html'));
+});
+
+router.post('/uploadImage', upload.single('imgfile'), function (req, res, next) {
+  res.send(req.file); // object를 리턴함
+})
+
+module.exports = router;
